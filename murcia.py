33627d7b8f3a57e09a0257b47cfec2089fc832e3
Biel <bpierasmorell@gmail.com>
2016-06-08 18:44:46 +0200

    Stores the data in mongoDB

--- murcia.py
+++ murcia.py
@@ -1,7 +1,10 @@
+# vim: set filencoding=UTF-8
+
 import requests
 import datetime
 import json
 import demjson
+from pymongo import MongoClient
 
 def connect():
     url = "http://sinqlair.carm.es/calidadaire/obtener_datos.aspx?tipo=tablaRedVigilancia"
@@ -22,29 +25,52 @@ def connect():
     query_type = "tipoConsulta=medias_horarias"
     stations="estacionesSelec=e_alcantarilla_Alcantarilla-e_aljorra_Aljorra-e_alumbres_Alumbres-e_caravaca_Caravaca-e_lorca_Lorca-e_mompean_Mompe%C3%A1n-e_sanbasilio_San%20Basilio-e_valle_Valle%20de%20Escombreras-eh_benizar_Ben%C3%ADzar-eh_launion_La%20Uni%C3%B3n-eh_sangines_San%20Gin%C3%A9s"
     pollutants="contaminantesSelec=cc_C6H6*Ben_Benceno_ug%2Fm3-cc_CO_CO_mg%2Fm3-cc_NO_NO_ug%2Fm3-cc_NO2_NO2_ug%2Fm3-cc_O3_O3_ug%2Fm3-cc_PM10_PM10_%26micro%3Bg%2Fm3-cc_SO2_SO2_ug%2Fm3-cc_C7H8*Tol_Tolueno_ug%2Fm3-cc_XIL*Xil_Xileno_ug%2Fm3-cm_HR_Humedad%20Relativa_%25%20Hr-cm_TMP_Temperatura_%C2%BA%20C"
-    today = datetime.date.today()
-    tomorrow = today + datetime.timedelta(days=1)
-    time_period ="periodoConsulta=sel_hoy&fechaInicioConsulta="+str(today.day)+"%2F"+str(today.month)+"%2F"+str(today.year)+"&fechaFinConsulta="+str(tomorrow.day)+"%2F"+str(tomorrow.month)+"%2F"+str(tomorrow.year)
+    now = datetime.datetime.now()
+    tomorrow = now + datetime.timedelta(days=1)
+    time_period ="periodoConsulta=sel_hoy&fechaInicioConsulta="+str(now.day)+"%2F"+str(now.month)+"%2F"+str(now.year)+"&fechaFinConsulta="+str(tomorrow.day)+"%2F"+str(tomorrow.month)+"%2F"+str(tomorrow.year)
     data_type = "tipo_dato=grid"
 
     data_string = query_type+"&"+stations+"&"+pollutants+"&"+time_period+"&"+data_type
 
 
-    data = {
-        "tipoConsulta": "medias_horarias",
-        "estaciones_Selec": "e_alcantarilla_Alcantarilla-e_aljorra_Aljorra-e_alumbres_Alumbres-e_caravaca_Caravaca-e_lorca_Lorca-e_mompean_Mompe%C3%A1n-e_sanbasilio_San%20Basilio-e_valle_Valle%20de%20Escombreras-eh_benizar_Ben%C3%ADzar-eh_launion_La%20Uni%C3%B3n-eh_sangines_San%20Gin%C3%A9s",
-        "contaminantesSelec": "cc_C6H6*Ben_Benceno_ug%2Fm3-cc_CO_CO_mg%2Fm3-cc_NO_NO_ug%2Fm3-cc_NO2_NO2_ug%2Fm3-cc_O3_O3_ug%2Fm3-cc_PM10_PM10_%26micro%3Bg%2Fm3-cc_SO2_SO2_ug%2Fm3-cc_C7H8*Tol_Tolueno_ug%2Fm3-cc_XIL*Xil_Xileno_ug%2Fm3-cm_HR_Humedad%20Relativa_%25%20Hr-cm_TMP_Temperatura_%C2%BA%20C",
-        "periodo_consulta": "sel_hoy",
-        "fechaInicioConsulta": "06%2F06%2F2016",
-        "fechaFinConsulta": "07%2F06%2F2016",
-        "tipo_dato": "grid"
-    }
-
     r = requests.post(url, data=data_string, headers=headers)
+    
     #We format the JSON response to be RFC4627 compliant
-    formatted_json = demjson.decode(r.text[3:].replace("'","\""))
-    formatted_json = json.dumps(formatted_json)
-    print formatted_json
-    print("\n\n\n"+str(len(formatted_json)))
+    formatted_json_string = demjson.decode(r.text[3:].replace("'","\""))
+    formatted_json_string = json.dumps(formatted_json_string)
+    formatted_json = json.loads(formatted_json_string)
+    
+    time_format = "%d/%m/%Y %H:00"
+    
+    #MongoDB information
+    client = MongoClient()
+    db = client['Estacions']
+    coll = db['Murcia']
+
+    for row in formatted_json:
+        date_field = row["fecha"]
+        if date_field == now.strftime(time_format):
+            for key in row.keys():
+                if key!="contaminante" and key!="fecha" and row[key] != "---":
+                    json_insert={
+                        "nombre": str(key),
+                        "fecha": datetime.datetime.strptime(row["fecha"],time_format),
+                        "tipo_dato": row["contaminante"],
+                        "valor": row[key]
+                    }
+                    #Units depend on the pollutants
+                    json_units={}
+                    if row["contaminante"] == "CO":
+                        json_units = {"unidades": "mg/m3"}
+                    elif row["contaminante"] == "TMP":
+                        json_units = {"unidades": "°C"}
+                    elif row["contaminante"] == "HR":
+                        json_units = {"unidades": "%"}
+                    else:
+                        json_units = {"unidades": "µg/m3"}
+                    #Joining the two JSON objects
+                    json_insert = {key: value for (key, value) in (json_insert.items() + json_units.items())}
+                    coll.insert_one(json_insert)
+
 
 connect()
